8f215ddc1f7c8db78f9bd66287eac95b608ec40e
bgizaw17 <50852631+bgizaw17@users.noreply.github.com>
2019-06-22 22:41:12 -0400

    Add files via upload

--- pypoll.py
+++ pypoll.py
@@ -1,89 +1,168 @@
-# Incorporated the csv module
-import csv
-
-# Files to load and output (Remember to change these)
-file_to_load = "raw_data/election_data_2.csv"
-file_to_output = "analysis/election_analysis_2.txt"
-
-# Total Vote Counter
-total_votes = 0
-
-# Candidate Options and Vote Counters
-candidate_options = []
-candidate_votes = {}
-
-# Winning Candidate and Winning Count Tracker
-winning_candidate = ""
-winning_count = 0
-
-# Read the csv and convert it into a list of dictionaries
-with open(file_to_load) as election_data:
-    reader = csv.DictReader(election_data)
-
-    # For each row...
-    for row in reader:
-
-        # Run the loader animation
-        print(". ", end=""),
-
-        # Add to the total vote count
-        total_votes = total_votes + 1
-
-        # Extract the candidate name from each row
-        candidate_name = row["Candidate"]
-
-        # If the candidate does not match any existing candidate...
-        # (In a way, our loop is "discovering" candidates as it goes)
-        if candidate_name not in candidate_options:
-
-            # Add it to the list of candidates in the running
-            candidate_options.append(candidate_name)
-
-            # And begin tracking that candidate's voter count
-            candidate_votes[candidate_name] = 0
-
-        # Then add a vote to that candidate's count
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-
-# Print the results and export the data to our text file
-with open(file_to_output, "w") as txt_file:
-
-    # Print the final vote count (to terminal)
-    election_results = (
-        f"\n\nElection Results\n"
-        f"-------------------------\n"
-        f"Total Votes: {total_votes}\n"
-        f"-------------------------\n")
-    print(election_results, end="")
-
-    # Save the final vote count to the text file
-    txt_file.write(election_results)
-
-    # Determine the winner by looping through the counts
-    for candidate in candidate_votes:
-
-        # Retrieve vote count and percentage
-        votes = candidate_votes.get(candidate)
-        vote_percentage = float(votes) / float(total_votes) * 100
-
-        # Determine winning vote count and candidate
-        if (votes > winning_count):
-            winning_count = votes
-            winning_candidate = candidate
-
-        # Print each candidate's voter count and percentage (to terminal)
-        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
-        print(voter_output, end="")
-
-        # Save each candidate's voter count and percentage to text file
-        txt_file.write(voter_output)
-
-    # Print the winning candidate (to terminal)
-    winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"-------------------------\n")
-    print(winning_candidate_summary)
-
-    # Save the winning candidate's name to the text file
-    txt_file.write(winning_candidate_summary)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "-------------------------\n",
+      "Election Results\n",
+      "-------------------------\n",
+      "Total Votes :3521001\n",
+      "-------------------------\n",
+      "O'Tooley: 2.999999147969569% (105630)\n",
+      "Li: 13.999996023857989% (492940)\n",
+      "Khan: 63.00001050837531% (2218231)\n",
+      "Correy: 19.999994319797125% (704200)\n",
+      "-------------------------\n",
+      "The winner is: Khan\n",
+      "-------------------------\n"
+     ]
+    }
+   ],
+   "source": [
+    "import os\n",
+    "import csv\n",
+    "\n",
+    "#Set the path for the CSV file\n",
+    "\n",
+    "PyPollcsv = os.path.join(\"Resources\",\"election_data.csv\")\n",
+    "\n",
+    "#Create the lists to store data. \n",
+    "\n",
+    "count = 0\n",
+    "candidatelist = []\n",
+    "unique_candidate = []\n",
+    "vote_count = []\n",
+    "vote_percent = []\n",
+    "\n",
+    "with open(PyPollcsv, newline=\"\") as csvfile:\n",
+    "\n",
+    "    csvreader = csv.reader(csvfile, delimiter=\",\")\n",
+    "\n",
+    "    csv_header = next(csvreader)\n",
+    "\n",
+    "    # Conduct the ask\n",
+    "\n",
+    "    for row in csvreader:\n",
+    "\n",
+    "        #Count the total number of votes\n",
+    "\n",
+    "        count = count + 1\n",
+    "\n",
+    "        #Set the candidate names to candidatelist\n",
+    "\n",
+    "        candidatelist.append(row[2])\n",
+    "\n",
+    "        #Create a set from the candidatelist to get the unique candidate names\n",
+    "\n",
+    "    for x in set(candidatelist):\n",
+    "\n",
+    "        unique_candidate.append(x)\n",
+    "\n",
+    "        # y is the total number of votes per candidate\n",
+    "\n",
+    "        y = candidatelist.count(x)\n",
+    "\n",
+    "        vote_count.append(y)\n",
+    "\n",
+    "        # z is the percent of total votes per candidate\n",
+    "\n",
+    "        z = (y/count)*100\n",
+    "\n",
+    "        vote_percent.append(z)\n",
+    "\n",
+    "        \n",
+    "\n",
+    "    winning_vote_count = max(vote_count)\n",
+    "\n",
+    "    winner = unique_candidate[vote_count.index(winning_vote_count)]\n",
+    "\n",
+    "    \n",
+    "\n",
+    "print(\"-------------------------\")\n",
+    "\n",
+    "print(\"Election Results\")   \n",
+    "\n",
+    "print(\"-------------------------\")\n",
+    "\n",
+    "print(\"Total Votes :\" + str(count))    \n",
+    "\n",
+    "print(\"-------------------------\")\n",
+    "\n",
+    "for i in range(len(unique_candidate)):\n",
+    "\n",
+    "            print(unique_candidate[i] + \": \" + str(vote_percent[i]) +\"% (\" + str(vote_count[i])+ \")\")\n",
+    "\n",
+    "print(\"-------------------------\")\n",
+    "\n",
+    "print(\"The winner is: \" + winner)\n",
+    "\n",
+    "print(\"-------------------------\")\n",
+    "\n",
+    "\n",
+    "\n",
+    "# Print to a text file: election_results.txt\n",
+    "\n",
+    "with open('election_results.txt', 'w') as text:\n",
+    "\n",
+    "    text.write(\"Election Results\\n\")\n",
+    "\n",
+    "    text.write(\"---------------------------------------\\n\")\n",
+    "\n",
+    "    text.write(\"Total Vote: \" + str(count) + \"\\n\")\n",
+    "\n",
+    "    text.write(\"---------------------------------------\\n\")\n",
+    "\n",
+    "    for i in range(len(set(unique_candidate))):\n",
+    "\n",
+    "        text.write(unique_candidate[i] + \": \" + str(vote_percent[i]) +\"% (\" + str(vote_count[i]) + \")\\n\")\n",
+    "\n",
+    "    text.write(\"---------------------------------------\\n\")\n",
+    "\n",
+    "    text.write(\"The winner is: \" + winner + \"\\n\")\n",
+    "\n",
+    "    text.write(\"---------------------------------------\\n\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
